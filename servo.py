4435e6c6cd96a341c5618509527859ea85dcd404
lf <lf-@users.noreply.github.com>
2016-06-11 00:49:45 -0600

    Clean up implementation of servos

--- servo.py
+++ servo.py
@@ -1,31 +1,33 @@
-import RPIO.PWM.Servo as RServo
+from RPIO.PWM import Servo as RServo
+
+servo = RServo()
 
 class Servo:
 
-	servo = RServo()
-	
 	def __init__(self, pin):
 		self.pin = pin
+		self._rate = None
+		self._angle = None
 	
 	@property
 	def rate(self):
-		return self.rate
+		return self._rate
 
 	@rate.setter
 	def rate(self, ms):
 		self.angle = (ms - 1000) * (9/50)
 		self.rate = ms
-		self.servo.set_servo(self.pin, self.rate)
+		servo.set_servo(self.pin, self.rate)
 
 	@property
 	def angle(self):
-		return self.angle
+		return self._angle
 
 	@angle.setter
 	def angle(self, angle):
 		self.angle = angle
 		self.rate = ((50/9.0)*angle) + 1000
-		self.servo.set_servo(self.pin, self.rate)
+		servo.set_servo(self.pin, self.rate)
 
 	def stop(self):
 		servo.stop_servo(self.pin)
