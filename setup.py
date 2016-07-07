0d631b0b172ab3ed20a4b020d8102eeacf807b95
parrottq <quinn.parrott@gmail.com>
2016-07-07 22:06:20 +0000

    Fixed package building

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name="Robotics",
     version="1.0",
     description="A Robotics Library",
     author="Lucas Fink, Quinn Parrott",
-    url="github.com/lf-/robotics"
+    url="github.com/lf-/robotics",
+    packages=["robotics"]
 )
 
