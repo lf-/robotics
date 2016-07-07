ef8dcffd563a8ca341d15373d8f5f91d36634e10
parrottq <quinn.parrott@gmail.com>
2016-07-07 22:00:09 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="Robotics",
+    version="1.0",
+    description="A Robotics Library",
+    author="Lucas Fink, Quinn Parrott",
+    url="github.com/lf-/robotics"
+)
+
