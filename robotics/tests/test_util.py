5a94334af69b3d16951155e40c1a4f70e4b51d45
lf- <lf-@users.noreply.github.com>
2016-07-15 21:54:11 -0600

    Improve util unit test

--- robotics/tests/test_util.py
+++ robotics/tests/test_util.py
@@ -3,3 +3,5 @@ from .. import util
 
 def test_translate():
     assert util.translate(0, -1, 1, 0, 10) == 5
+    assert util.translate(1, -1, 1, 0, 10) == 10
+    assert util.translate(-1, -1, 1, 0, 10) == 0
