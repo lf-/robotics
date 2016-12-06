6e581c04830718be8ddfddbcd33ef5a2bf529f1f
lf- <lf-@users.noreply.github.com>
2016-12-06 16:52:25 -0600

    Load modules only when we know we're using them
    This is probably terrible.

--- robotics/__init__.py
+++ robotics/__init__.py
@@ -1,13 +1,11 @@
-from . import controller
-from . import jsonapi
-
-
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('frontend', choices=('controller', 'jsonapi'))
     args = parser.parse_args()
     if args.frontend == 'controller':
+        from . import controller
         controller.main()
     elif args.frontend == 'jsonapi':
+        from . import jsonapi
         jsonapi.main()
